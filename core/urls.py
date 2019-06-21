95e256d313287d63f11b0ee1aa3cda7d4b682e7b
amit <amit.kadivar1@gmail.com>
2019-06-21 23:36:21 +0530

    add view

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import item_list, checkout, product, HomeView, ItemDetailView, add_to_cart,remove_from_cart
+
+app_name = 'core'
+urlpatterns = [
+    path('', HomeView.as_view(), name='item-list'),
+    path('checkout/', checkout, name='checkoutpage'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='productpage'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+
+
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+from .models import Item, Order, OrderItem
+from django.contrib import messages
+from django.utils import timezone
 # Create your views here.
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home-page.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product-page.html'
+
+
+def item_list(request):
+    context = {'items': Item.objects.all()}
+    return render(request, 'home-page.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout-page.html', {})
+
+
+def product(request):
+    return render(request, 'product-page.html', {})
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item, user=request.user, ordered=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, 'Cart Updated')
+            return redirect("core:productpage", slug=slug)            
+        else:
+            order.items.add(order_item)
+            messages.info(request, 'Add To Cart')
+            return redirect("core:productpage", slug=slug)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "Add To Cart")
+        return redirect("core:productpage", slug=slug)
+
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item, user=request.user, ordered=False)[0]
+            order.items.remove(order_item)
+            messages.info(request, "Remove From Cart")
+            return redirect("core:productpage", slug=slug)
+
+        else:
+            messages.info(request, "Not Exist in cart")
+            return redirect("core:productpage", slug=slug)
+    else:
+        messages.info(request, "Not Any Active Order")
+        return redirect("core:productpage", slug=slug)
+        
+    return redirect("core:productpage", slug=slug)
