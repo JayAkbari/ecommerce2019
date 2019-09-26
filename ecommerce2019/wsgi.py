23272bc4f56f0400050b23eac20174ccff173cf7
amit <amit.kadivar1@gmail.com>
2019-09-26 08:41:34 +0530

    changes in wsgi

--- ecommerce2019/wsgi.py
+++ ecommerce2019/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce2019.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce2019.settings.production')
 
 application = get_wsgi_application()
